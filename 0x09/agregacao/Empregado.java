50b0e9489d7e8f3c72183602133756e65928eb79
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-06-16 16:01:30 -0300

    testes

--- 0x09/agregacao/Empregado.java
+++ 0x09/agregacao/Empregado.java
@@ -39,7 +39,7 @@ public class Empregado {
         System.out.println("Nome: " + nome);
         System.out.println("Bairro: " + endereco.getBairro());
         System.out.println("Cidade: " + endereco.getCidade());
-        System.out.println("Pais: " + endereco.getPais());
+        System.out.printf("Pais: " + endereco.getPais());
     }
 
 }
