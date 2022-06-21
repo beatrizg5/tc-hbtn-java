0cddde605ab6c36d377451c628c41a0f34e1d57c
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-06-21 13:31:21 -0300

    testes

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -1,7 +1,9 @@
 public class Numeros0a99 {
+    static final int MAX_NUM = 99;
+
     public static void main(String[] args) {
-        for (int i = 99; i > -1; i--) {
-            System.out.printf(i + ", ");
+        for(int i=0; i<=MAX_NUM; i++){
+            System.out.printf("%d%c ",i, i==MAX_NUM ? 00: 44);
         }
     }
 }
