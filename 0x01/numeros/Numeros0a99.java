c4e83fc596e53a8d5e26518fb6eea9be29a31838
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-06-21 23:47:23 -0300

    testes

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -2,9 +2,9 @@ public class Numeros0a99 {
     public static void main(String[] args) {
         for(int i = 0; i<=99; i++){
             if(i>0){
-                System.out.print("");
+                System.out.print(", ");
             }
-            System.out.printf("%d%c ",i, i==99 ? 0 : 44);
+            System.out.printf("%d",i, i==99 ? 0 : 44);
         }
     }
 }
