57f35995ac6516d6600035ae9c9d403124d00d38
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-06-22 09:33:48 -0300

    testes

--- 0x01/pares/NumerosPares.java
+++ 0x01/pares/NumerosPares.java
@@ -1,13 +1,11 @@
 public class NumerosPares {
     public static void main(String args[]) {
         int valor;
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 99; i++) {
             if (i % 2 == 0) {
-                if(i>0){
-                System.out.printf(", ");
-                    }
-                System.out.print(i);
+                System.out.printf("%d%s", i, i == 98 ? "" : ", ");
             }
         }
+        System.out.println();
     }
 }
