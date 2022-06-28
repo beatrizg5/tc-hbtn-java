ce0ca18faef7cfc5d7e6d1f2a58c92d1e4a9c70d
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-06-28 08:50:50 -0300

    teste

--- Heranca_Exercicios/resumo_pedido/Pedido.java
+++ Heranca_Exercicios/resumo_pedido/Pedido.java
@@ -1,8 +1,9 @@
 import produtos.Dvd;
 import produtos.Livro;
+import java.util.Locale;
 
 public class Pedido {
-
+    Locale locale = new Locale("pt","BR");
     private double percentualDesconto;
     private ItemPedido[] itens;
 
@@ -16,15 +17,15 @@ public class Pedido {
         System.out.println("------- RESUMO PEDIDO -------");
         for (ItemPedido iten : itens) {
 
-            System.out.printf("Tipo: %s Titulo: %s Preco: %.2f Quant: %d Total: %.2f\n",
+            System.out.printf(locale,"Tipo: %s Titulo: %s Preco: %.2f Quant: %d Total: %.2f\n",
                     iten.getProduto().getClass().getSimpleName(), iten.getProduto().getTitulo(),
                     iten.getProduto().obterPrecoLiquido(), iten.getQuatidade(), (iten.getProduto().obterPrecoLiquido() * iten.getQuatidade()));
         }
         System.out.println("----------------------------");
-        System.out.printf("DESCONTO: %.2f\n", getPercentualDesconto());
-        System.out.printf("TOTAL PRODUTOS: %.2f\n", totalProdutos());
+        System.out.printf(locale,"DESCONTO: %.2f\n", getPercentualDesconto());
+        System.out.printf(locale,"TOTAL PRODUTOS: %.2f\n", totalProdutos());
         System.out.println("----------------------------");
-        System.out.printf("TOTAL PEDIDO: %.2f\n", calcularTotal());
+        System.out.printf(locale,"TOTAL PEDIDO: %.2f\n", calcularTotal());
         System.out.println("----------------------------");
     }
 
