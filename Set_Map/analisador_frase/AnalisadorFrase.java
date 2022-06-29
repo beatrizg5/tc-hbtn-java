c7838ebe8bf4847ab254c4dce27e845e80f74bc9
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-06-29 17:14:27 -0300

    teste

--- Set_Map/analisador_frase/AnalisadorFrase.java
+++ Set_Map/analisador_frase/AnalisadorFrase.java
@@ -6,7 +6,7 @@ import java.util.TreeMap;
 public class AnalisadorFrase {
 
     public static TreeMap contagemPalavras(String texto) {
-        String[] palavras = texto.toLowerCase(Locale.ROOT).split("[?,.! ]+");
+        String[] palavras = texto.toLowerCase(Locale.ROOT).split("[?.! ]+");
         TreeMap<String, Integer> mapa = new TreeMap<String, Integer>();
         for (int i = 0; i < palavras.length; i++) {
         if (mapa.isEmpty()) {
