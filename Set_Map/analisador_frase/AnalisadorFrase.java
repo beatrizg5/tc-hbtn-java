00be9288001e45a20ba6cb57d78421aea1842c5c
beatrizgbciandt <107229954+beatrizgbciandt@users.noreply.github.com>
2022-06-29 18:59:40 -0300

    Update AnalisadorFrase.java

--- Set_Map/analisador_frase/AnalisadorFrase.java
+++ Set_Map/analisador_frase/AnalisadorFrase.java
@@ -1,11 +1,9 @@
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
 import java.util.TreeMap;
 
 public class AnalisadorFrase {
 
-    public static TreeMap contagemPalavras(String texto) {
+    public static TreeMap<String, Integer>contagemPalavras(String texto) {
         String[] palavras = texto.toLowerCase(Locale.ROOT).split("[?.! ]+");
         TreeMap<String, Integer> mapa = new TreeMap<String, Integer>();
         for (int i = 0; i < palavras.length; i++) {
