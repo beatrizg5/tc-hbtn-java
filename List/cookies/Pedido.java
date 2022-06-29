753a99b2c9b807c40cbd99b558009d6ee9205c57
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-06-28 23:43:33 -0300

    teste

---
+++ List/cookies/Pedido.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Pedido {
+    public Pedido() {
+        ArrayList<PedidoCookie>cookies = new ArrayList<>();
+    }
+    public void adicionarPedidoCookie(PedidoCookie chocolate){
+
+    }
+    public Object obterTotalCaixas(){
+
+        return null;
+    }
+    public int removerSabor(String chocolate){
+
+        return 0;
+    }
+}
