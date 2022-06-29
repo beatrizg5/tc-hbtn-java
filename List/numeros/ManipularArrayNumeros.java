e3ef00552e68a62e076bd2963b862513868ccff0
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-06-28 23:08:16 -0300

    teste

---
+++ List/numeros/ManipularArrayNumeros.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ManipularArrayNumeros {
+
+
+    static public void buscarPosicaoNumero() {
+        ArrayList<Integer> numeros = new ArrayList<Integer>();
+
+    }
+
+    static public void adicionarNumero() {
+
+    }
+
+    public static void removerNumero(List<Integer> numeros, int i) {
+    }
+
+    public static void substituirNumero(List<Integer> numeros, int i, int i1) {
+    }
+
+    public static void adicionarNumero(List<Integer> numeros, int i) {
+    }
+}
