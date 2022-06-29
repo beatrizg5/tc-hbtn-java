2584351a9c46403a8dd070641e6197f23fb350b8
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-06-29 17:10:04 -0300

    teste

---
+++ Set_Map/analisador_frase/AnalisadorFrase.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class AnalisadorFrase {
+
+    public static TreeMap contagemPalavras(String texto) {
+        String[] palavras = texto.toLowerCase(Locale.ROOT).split("[?,.! ]+");
+        TreeMap<String, Integer> mapa = new TreeMap<String, Integer>();
+        for (int i = 0; i < palavras.length; i++) {
+        if (mapa.isEmpty()) {
+            mapa.put(palavras[0], 1);
+        } else {
+                if(mapa.containsKey(palavras[i])){
+                    Integer aux = mapa.get(palavras[i]);
+                    mapa.replace(palavras[i],aux +1);
+                }else{
+                    mapa.put(palavras[i], 1);
+                }
+            }
+        }
+
+        return mapa;
+    }
+}
