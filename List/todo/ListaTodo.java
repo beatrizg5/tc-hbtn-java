ba495a615f67b324eaf7b49987891584791e51a8
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-06-29 00:16:28 -0300

    teste

--- List/todo/ListaTodo.java
+++ List/todo/ListaTodo.java
@@ -6,7 +6,7 @@ public class ListaTodo {
     }
 
     public void adicionarTarefa(Tarefa ler_conteudo_aula_lambda){
-        //throw new IllegalArgumentException("Tarefa com identificador <identificador> ja existente na lista");
+        throw new IllegalArgumentException("Tarefa com identificador <identificador> ja existente na lista");
     }
 
     public boolean marcarTarefaFeita(int i){
@@ -23,30 +23,6 @@ public class ListaTodo {
                 "    [X]  Id: 1 - Descricao: Ler conteudo aula Lambda\n" +
                 "    [ ]  Id: 2 - Descricao: Aprender Design Patterns\n" +
                 "    [ ]  Id: 3 - Descricao: Limpar o quarto\n" +
-                "    ----\n" +
-                "    [ ]  Id: 1 - Descricao: Ler conteudo aula Lambda\n" +
-                "    [X]  Id: 2 - Descricao: Aprender Design Patterns\n" +
-                "    [ ]  Id: 3 - Descricao: Limpar o quarto\n" +
-                "    ----\n" +
-                "    [ ]  Id: 1 - Descricao: Ler conteudo aula Lambda\n" +
-                "    [X]  Id: 2 - Descricao: Aprender Design Patterns\n" +
-                "    [ ]  Id: 3 - Descricao: Limpar o quarto\n" +
-                "    ----\n" +
-                "    [X]  Id: 1 - Descricao: Ler conteudo aula Lambda\n" +
-                "    [X]  Id: 2 - Descricao: Aprender Design Patterns\n" +
-                "    [X]  Id: 3 - Descricao: Limpar o quarto\n" +
-                "    ----\n" +
-                "    [ ]  Id: 1 - Descricao: Ler conteudo aula Lambda\n" +
-                "    [ ]  Id: 2 - Descricao: Aprender Design Patterns\n" +
-                "    [ ]  Id: 3 - Descricao: Limpar o quarto\n" +
-                "    ----\n" +
-                "    false\n" +
-                "    ----\n" +
-                "    false\n" +
-                "    ----\n" +
-                "    Tarefa com identificador 2 ja existente na lista\n" +
-                "    ----\n" +
-                "    Descricao de tarefa invalida\n" +
-                "    ----");
+                "    ----\n");
     }
 }
