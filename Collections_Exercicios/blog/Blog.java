5b213d571e973245b354670cae62f2de244b795e
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-06-30 15:09:56 -0300

    test

---
+++ Collections_Exercicios/blog/Blog.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Blog {
+    List<Post> listaDePosts;
+
+    public Blog() {
+        listaDePosts = new ArrayList<>();
+    }
+
+    public void adicionarPostagem(Post postagem) {
+        listaDePosts.add(postagem);
+    }
+
+    public Set<String> obterTodosAutores() {
+        Set<String> listaDeAutores = new TreeSet<>();
+        for (int i = 0; i < listaDePosts.size(); i++) {
+            listaDeAutores.add(listaDePosts.get(i).getAutor());
+
+        }
+        return listaDeAutores;
+    }
+
+    public Map<String, Integer> obterContagemPorCategoria() {
+        Map<String, Integer> listaDeCategoria = new TreeMap<>();
+        for (int i = 0; i < listaDePosts.size(); i++) {
+            if (listaDeCategoria.isEmpty()) {
+                listaDeCategoria.put(listaDePosts.get(i).getCategoria(), 1);
+            } else {
+                for (int j = 0; j < listaDeCategoria.size(); j++) {
+                    if (listaDeCategoria.containsKey(listaDePosts.get(i).getCategoria())) {
+                        Integer valor = listaDeCategoria.get(listaDePosts.get(i).getCategoria());
+                        listaDeCategoria.replace(listaDePosts.get(i).getCategoria(), valor + 1);
+                        break;
+                    }else {
+                        listaDeCategoria.put(listaDePosts.get(i).getCategoria(), 1);
+                        break;
+                    }
+                }
+            }
+        }return listaDeCategoria;
+    }
+}
