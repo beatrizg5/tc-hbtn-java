567d5d1e2f9da1ca550a3d834a5941e2a2c2d2c4
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-07-14 11:16:50 -0300

    teste

---
+++ Streams_Collector_Optional_Exercicios/hobbies_por_cargo/Pessoa.java
@@ -0,0 +1,53 @@
+import java.util.List;
+
+public class Pessoa implements Comparable<Pessoa> {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+    private List<String>hobbies;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario, List<String> hobbies) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+        this.hobbies = hobbies;
+    }
+
+    @Override
+    public int compareTo(Pessoa pessoa) {
+        return this.nome.compareTo(pessoa.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d] %s %s %d R$ %.6f",codigo, nome, cargo, idade, salario);
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public List<String> getHobbies() {
+        return hobbies;
+    }
+}
